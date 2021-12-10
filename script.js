ea3fcb667b8c5ea864eea1e4c3f280be69320e2e
Rajkumar Murugesan <90443069+rajm688@users.noreply.github.com>
2021-12-10 11:13:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-document.getElementById("submit").addEventListener("click",clicked);
-function clicked(){
-    var value = document.getElementById("input").value;
+document.getElementById("submit").addEventListener("click",clicked); //selecting the submit button and adding Event listener
+function clicked(){ // function for eventlistener
+    var value = document.getElementById("input").value; // collecting the entered number
     var url = `https://api-thirukkural.vercel.app/api?num=${value}`
     let fetchData = async ()=>{
-                let response = await fetch(url);
+                let response = await fetch(url); //fetching Data
                 let data = await response.json();
                 let num = data.number;
-                let athigaram = data.sect_tam
+                let athigaram = data.sect_tam // collecting Necessary data from API
                 let title= data.chap_tam
                 let lin1 = data.line1
                 let lin2 = data.line2
                 let exp = data.tam_exp
-                let main = document.createElement("div")
+                let main = document.createElement("div") //creating the Div elements
                 main.className="container"
-                main.innerHTML = `<strong>Thirukkural Number:</strong><i>${num}</i><br><br>
+                main.innerHTML = `<strong>Thirukkural Number:</strong><i>${num}</i><br><br> // appending the datas
                 <strong>Adhikaram:   </strong><i>${athigaram}</i><br><br>
                 <strong>Thallipu:   </strong><i>${title}</i><br><br>
                 <strong>ThirukKural:   <br></strong><i>${lin1},<br>${lin2}</i><br><br>
